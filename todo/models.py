52f38fe3adb77b2227900d83f731ad6bbe0ddb15
kenzoukenzou <kenzou.kenzou104809@gmail.com>
2020-02-08 17:20:56 +0900

    feat: Todoモデルに優先度と締め切りのカラムを追加する修正&migrate実行

--- todo/models.py
+++ todo/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 
+
+PRIORITY = (('danger', 'high'), ('infor', 'normal'), ('success', 'low'))
 # Create your models here.
 # @see https://qiita.com/nachashin/items/f768f0d437e0042dd4b3
 class TodoModel(models.Model):
     title = models.CharField(max_length=100)
     memo = models.TextField()
-
+    priority = models.CharField(
+        max_length = 50,
+        choices = PRIORITY
+    )
+    duedate = models.DateField()
     def __str__(self):
         return self.title
