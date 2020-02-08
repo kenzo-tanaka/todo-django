18012d7eadb109f833ba16c1db7f1db5485dedcf
kenzoukenzou <kenzou.kenzou104809@gmail.com>
2020-02-08 16:03:23 +0900

    fix: Todoテーブルを作成するためにmodels.py修正・リファレンスをコメント付与

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+# @see https://qiita.com/nachashin/items/f768f0d437e0042dd4b3
+class TodoModel(models.Model):
+    title = models.CharField(max_length=100)
+    memo = models.TextField()
